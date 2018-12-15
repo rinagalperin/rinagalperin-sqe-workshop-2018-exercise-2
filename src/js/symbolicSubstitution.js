c62ea4a0be62f0b278fe596dd99b5cc448a72ad9
Rina Galperin <rina@h-MacBook-Air-sl-Rina.local>
2018-12-15 11:42:58 +0200

    get entry value & update expression

--- src/js/symbolicSubstitution.js
+++ src/js/symbolicSubstitution.js
@@ -9,7 +9,7 @@ let result;
 let router;
 let symbol_table;
 let parsed_code;
-let original_lines;
+let lines;
 
 function SymbolicSubstitution(functionCode){
     result = []; // final function to be displayed
@@ -17,18 +17,18 @@ function SymbolicSubstitution(functionCode){
 
     router = InitRouter();
     parsed_code = esprima.parseScript(functionCode, {loc:true});
-    original_lines = functionCode.split('\n');
+    lines = functionCode.split('\n');
 
-    let function_globals_indexes = GlobalVariablesHandler(symbol_table, parsed_code, original_lines);
+    let function_globals_indexes = GlobalVariablesHandler(symbol_table, parsed_code);
     ParseFunctionUnit(parsed_code.body[function_globals_indexes.function]);
     parsed_code = parsed_code.body[function_globals_indexes.function];
 
-    Route(symbol_table, parsed_code, original_lines);
+    Route(symbol_table, parsed_code);
 
     return result;
 }
 
-function GlobalVariablesHandler(symbol_table, parsed_code, lines){
+function GlobalVariablesHandler(symbol_table, parsed_code){
     let ans = {};
     ans.globals = [];
     ans.function = 0;
@@ -40,23 +40,23 @@ function GlobalVariablesHandler(symbol_table, parsed_code, lines){
 
     for(let global_index in ans.globals){
         let global_declaration = parsed_code.body[global_index];
-        GlobalDeclarationHandler(symbol_table, global_declaration, lines);
+        GlobalDeclarationHandler(symbol_table, global_declaration);
     }
 
     return ans;
 }
 
-function GlobalDeclarationHandler(symbol_table, parsed_code, lines){
+function GlobalDeclarationHandler(symbol_table, parsed_code){
     for(let declaration of parsed_code.declarations){
         symbol_table[declaration.id.name] = declaration.init != null ? declaration.init.value : '';
     }
 }
 
-function Route(symbol_table, parsed_code, lines){
-    router[parsed_code.type](symbol_table, parsed_code, lines);
+function Route(symbol_table, parsed_code){
+    router[parsed_code.type](symbol_table, parsed_code);
 }
 
-function FunctionDeclarationHandler(symbol_table, parsed_code, lines){
+function FunctionDeclarationHandler(symbol_table, parsed_code){
     let function_parameters = parsed_code.params;
     let function_body = parsed_code.body;
     let parameters_count = 1;
@@ -75,57 +75,44 @@ function FunctionDeclarationHandler(symbol_table, parsed_code, lines){
     }
 
     result.push(result_entry);
-    BlockStatementHandler(symbol_table, function_body, lines);
+    BlockStatementHandler(symbol_table, function_body);
 }
 
-function BlockStatementHandler(symbol_table, parsed_code, lines){
+function BlockStatementHandler(symbol_table, parsed_code){
     for(let i = 0; i < parsed_code.body.length; i++){
         let parsed_code_statement = parsed_code.body[i];
-        //console.log(parsed_code_statement.type);
-        router[parsed_code_statement.type](symbol_table, parsed_code_statement, lines);
+        console.log(parsed_code_statement.type);
+        router[parsed_code_statement.type](symbol_table, parsed_code_statement);
     }
 }
 
-function VariableDeclarationHandler(symbol_table, parsed_code, lines){
+function VariableDeclarationHandler(symbol_table, parsed_code){
     for(let declaration of parsed_code.declarations){
-        VariableDeclaratorHandler(symbol_table, declaration, lines);
+        VariableDeclaratorHandler(symbol_table, declaration);
     }
 }
 
-function VariableDeclaratorHandler(symbol_table, declarator, lines){
-    for(let e of entries){
-        if(e['Line'] === declarator.loc.start.line && e['Name'] === declarator.id.name){
-            symbol_table[declarator.id.name] = ExtractUpdatedValue(symbol_table, declarator.id.name, e['Value']);
-        }
-    }
+function VariableDeclaratorHandler(symbol_table, parsed_code){
+    let param = parsed_code.id.name;
+    SetEntryValue(parsed_code, param);
 }
 
-function ExpressionStatementHandler(symbol_table, expression_statement, lines){
-    router[expression_statement.expression.type](symbol_table, expression_statement.expression, lines);
+function ExpressionStatementHandler(symbol_table, expression_statement){
+    router[expression_statement.expression.type](symbol_table, expression_statement.expression);
+}
+
+function UpdateExpressionHandler(symbol_table, parsed_code){
+    let param = parsed_code.argument.name;
+    SetEntryValue(parsed_code, param);
+}
+
+function AssignmentExpressionHandler(symbol_table, parsed_code){
+    let param = parsed_code.left.name;
+    SetEntryValue(parsed_code, param);
 }
 
 // TODO:
-function UpdateExpressionHandler(symbol_table, update_expression_statement, lines){
-    console.log(update_expression_statement);
-    console.log(update_expression_statement.left);
-    console.log(update_expression_statement.right);
-    console.log(entries)
-}
-
-function AssignmentExpressionHandler(symbol_table, assignment_expression_statement, lines){
-    let param = assignment_expression_statement.left.name;
-
-    for(let e of entries){
-        if(e['Line'] === assignment_expression_statement.loc.start.line && e['Name'] === param){
-            symbol_table[param] = ExtractUpdatedValue(symbol_table, param, e['Value']);
-        }
-    }
-
-    console.log(symbol_table)
-}
-
-// TODO:
-function IfStatementHandler(symbol_table, parsed_code, lines){
+function IfStatementHandler(symbol_table, parsed_code){
     let symbol_table_tmp = {};
 
     Object.keys(symbol_table).forEach(function(symbol) {
@@ -134,12 +121,12 @@ function IfStatementHandler(symbol_table, parsed_code, lines){
 }
 
 // TODO:
-function WhileStatementHandler(symbol_table, parsed_code, lines){
+function WhileStatementHandler(symbol_table, parsed_code){
 
 }
 
 // TODO:
-function ReturnStatementHandler(symbol_table, parsed_code, lines){
+function ReturnStatementHandler(symbol_table, parsed_code){
 
 }
 
@@ -148,12 +135,20 @@ function ExtractUpdatedValue(symbol_table, param, new_assigned_value){
         let drill_down = new_assigned_value.split('*').join(',').split('-').join(',').split('+').join(',').split('/').join(',').split(' ').join(',').split(',');
         for(let d of drill_down){
             if(d !== "" && d in symbol_table){
-                new_assigned_value = new_assigned_value.replace(d, symbol_table[d]);
+                new_assigned_value = new_assigned_value.replace(d, '('+ symbol_table[d] + ')');
             }
         }
     }
 
     return new_assigned_value;
+}
+
+function SetEntryValue(parsed_code, param){
+    for(let e of entries){
+        if(e['Line'] === parsed_code.loc.start.line && e['Name'] === param){
+            symbol_table[param] = ExtractUpdatedValue(symbol_table, param, e['Value']);
+        }
+    }
 }
 
 function InitRouter(){
